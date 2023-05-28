268aa9f676089baf8ea63200e88cc1f2e781497d
Munoto <zeron.masimov@mail.ru>
2023-05-28 23:33:46 +0600

    Los Pollos Hermanos
    Ilkham Massimov

---
+++ Los-Pollos-Hermanos/backend/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        id: 1,
+        name: "Alitas de pollo 6x",
+        desc: "Delicious chicken wings in a spicy, crispy breading. Even tastier with BBQ sauce!",
+        price: 2,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685187157/los-pollos-hermanos/alitas-de-pollo_nsdj85.png",
+    },
+    {
+        id: 2,
+        name: "Alitas de pollo 12x (BIG)",
+        desc: "Delicious chicken wings in a spicy, crispy breading. Even tastier with BBQ sauce!",
+        price: 3 ,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685187157/los-pollos-hermanos/alitas-de-pollo_nsdj85.png",
+    },
+    {
+        id: 3,
+        name: "Albuquerque burger",
+        desc: "Albuquerque is a unique sandwich with a large chopped 100% selected beef steak.",
+        price: 3,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685187093/los-pollos-hermanos/albuquerque-burger_xeyzae.png",
+    },
+    {
+        id: 4,
+        name: "Gustavo burger",
+        desc: "Gustavo is a juicy chicken cutlet in a crispy breading, Iceberg lettuce, a special sauce.",
+        price: 2,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685279738/los-pollos-hermanos/gustavo-burger_eyyjym.png",
+    },
+    {
+        id: 5,
+        name: "Heisenberg burger",
+        desc: "Heisenberg burger - this is the same Burger with two large chopped 100% beef steaks on a sesame seed bun..",
+        price: 2,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685279870/los-pollos-hermanos/heisenberg-burger_um3mly.png",
+    },
+    {
+        id: 6,
+        name: "Jesse burger",
+        desc: "Cheeseburger is a minced whole beef steak with chunks of Cheddar cheese on a caramelized bun seasoned.",
+        price: 3,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685279994/los-pollos-hermanos/jesse-burger_yjlowf.png",
+    },
+    {
+        id: 7,
+        name: "Coca-cola",
+        desc: "Refreshing carbonated drink Coca-Cola in a bottle.",
+        price: 1,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685280638/los-pollos-hermanos/cocacola_vtw59t.png",
+    },
+    {
+        id: 8,
+        name: "Aqua",
+        desc: "Drinking sparkling water Aqua Minerale produced by PepsiCo.",
+        price: 1,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685280774/los-pollos-hermanos/aqua_cobdoj.png",
+    },
+    {
+        id: 9,
+        name: "Lipton",
+        desc: "Soft drink with lemon taste Lipton Ice Tea Lemon. Served in three versions.",
+        price: 1,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685280857/los-pollos-hermanos/lipton_ampmw3.png",
+    },
+    {
+        id: 10,
+        name: "Milkshake Strawberry",
+        desc: "Strawberry Milkshake is a superbly whipped smoothie made with high quality milk formula and strawberry syrup.",
+        price: 2,
+        image: "https://res.cloudinary.com/dsle0fder/image/upload/v1685280950/los-pollos-hermanos/milkcoctail_kylzpc.png",
+    },
+];
+
+module.exports = products;
